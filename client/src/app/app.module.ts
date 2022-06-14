2978f26ce31fed1a06cd36bc2bb78d67766e552e
Jean-Luc <jeanluc.bicksler@hcl.com>
2022-06-14 18:08:15 -0400

    Added photo gallery

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -20,7 +20,6 @@ import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
 import { MemberCardComponent } from './members/member-card/member-card.component';
 import { JwtInterceptor } from './_interceptors/jwt.interceptor';
-
 @NgModule({
   declarations: [
     AppComponent,
