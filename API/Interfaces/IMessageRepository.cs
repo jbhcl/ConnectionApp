b2b45ffc62becbdb360f5fc8e669387fe785b6f7
Jean-Luc <jeanluc.bicksler@hcl.com>
2022-06-20 14:52:57 -0400

    Basic message list UI

--- API/Interfaces/IMessageRepository.cs
+++ API/Interfaces/IMessageRepository.cs
@@ -1,6 +1,6 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using API.Data;
+using API.DTOs;
 using API.Entities;
 using API.Helpers;
 
@@ -11,8 +11,8 @@ namespace API.Interfaces
         void AddMessage(Message message);
         void DeleteMessage(Message message);
         Task<Message> GetMessage(int id);
-        Task<PagedList<MessageDto>> GetMessagesForUser();
-        Task<IEnumerable<MessageDto>> GetMessageThread(int currentUserId, int recipientId);
+        Task<PagedList<MessageDto>> GetMessagesForUser(MessageParams messageParams);
+        Task<IEnumerable<MessageDto>> GetMessageThread(string currentUsername, string recipientUsername);
         Task<bool> SaveAllAsync();
     }
 }
---
+++ client/src/app/_models/message.ts
@@ -0,0 +1,12 @@
+export interface Message {
+    id: number;
+    senderid: number;
+    senderUsername: string;
+    senderPhotoUrl: string;
+    recipientId: number;
+    recipientUsername: string;
+    recipientPhotoUrl: string;
+    content: string;
+    dateRead?: Date;
+    messageSent: Date;
+}
