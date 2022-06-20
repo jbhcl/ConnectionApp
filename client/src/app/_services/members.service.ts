d30e00375a5b1580e1265d827bf8eaf1898747b2
Jean-Luc <jeanluc.bicksler@hcl.com>
2022-06-20 03:16:56 -0400

    UI for sending out connection requests

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -83,6 +83,15 @@ export class MembersService {
   deletePhoto(photoId: number) {
     return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId);
   }
+
+  addLike(username: string) {
+    return this.http.post(this.baseUrl + 'likes/' + username, {});
+  }
+
+  getLikes(predicate: string) {
+    return this.http.get(this.baseUrl + 'likes?predicate=' + predicate);
+  }
+
   private getPaginatedResult<T>(url, params: HttpParams) {
     const paginatedResult: PaginatedResult<T> = new PaginatedResult<T>();
     return this.http.get<T>(url, { observe: 'response', params }).pipe(
