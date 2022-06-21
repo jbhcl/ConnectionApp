9df31729ae9c1cbb3c965bf176f28aedea39c4d9
Jean-Luc <jeanluc.bicksler@hcl.com>
2022-06-21 16:08:33 -0400

    Fix users auth

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -26,7 +26,6 @@ namespace API.Controllers
             _userRepository = userRepository;
         }
 
-        [Authorize(Roles = "Admin")]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers([FromQuery]UserParams userParams)
         {
