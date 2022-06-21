ed3bbb8bc584e5f7974268efbb854eaf2757d5bc
Jean-Luc <jeanluc.bicksler@hcl.com>
2022-06-21 11:03:41 -0400

    Add janny to db seeder

--- API/Program.cs
+++ API/Program.cs
@@ -21,8 +21,9 @@ namespace API
             try {
                 var context = services.GetRequiredService<DataContext>();
                 var userManager = services.GetRequiredService<UserManager<AppUser>>();
+                var roleManager = services.GetRequiredService<RoleManager<AppRole>>();
                 await context.Database.MigrateAsync();
-                await Seed.SeedUsers(userManager);
+                await Seed.SeedUsers(userManager, roleManager);
             } catch (Exception ex) {
                 var logger = services.GetRequiredService<ILogger<Program>>();
                 logger.LogError(ex, "An error occured during migration");
