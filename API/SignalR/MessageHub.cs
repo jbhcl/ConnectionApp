a81815104db39475065d311301aa65cfe38f08e5
Jean-Luc <jeanluc.bicksler@hcl.com>
2022-06-22 14:38:38 -0400

    Notify users of messages

--- API/SignalR/MessageHub.cs
+++ API/SignalR/MessageHub.cs
@@ -15,8 +15,15 @@ namespace API.SignalR
         private readonly IMapper _mapper;
         private readonly IMessageRepository _messageRepository;
         private readonly IUserRepository _userRepository;
-        public MessageHub(IMessageRepository messageRepository, IMapper mapper, IUserRepository userRepository)
+        private readonly IHubContext<PresenceHub> _presenceHub;
+        private readonly PresenceTracker _tracker;
+
+        public MessageHub(IMessageRepository messageRepository, IMapper mapper,
+            IUserRepository userRepository, IHubContext<PresenceHub> presenceHub,
+            PresenceTracker tracker)
         {
+            _tracker = tracker;
+            _presenceHub = presenceHub;
             _userRepository = userRepository;
             _messageRepository = messageRepository;
             _mapper = mapper;
@@ -67,8 +74,21 @@ namespace API.SignalR
 
             var group = await _messageRepository.GetMessageGroup(groupName);
 
-            if(group.Connections.Any(x => x.Username == recipient.UserName)) {
+            if (group.Connections.Any(x => x.Username == recipient.UserName))
+            {
                 message.DateRead = DateTime.UtcNow;
+            }
+            else
+            {
+                var connections = await _tracker.GetConnectionsForUser(recipient.UserName);
+                if (connections != null)
+                {
+                    await _presenceHub.Clients.Clients(connections).SendAsync("NewMessageReceived", new
+                    {
+                        username = sender.UserName,
+                        knownAs = sender.KnownAs
+                    });
+                }
             }
 
             _messageRepository.AddMessage(message);
