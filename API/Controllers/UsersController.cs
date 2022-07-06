ea3aba8c5e24006b4e2282e7c7b7750a28ef0475
Your Name <you@example.com>
2022-07-06 15:21:14 -0400

    Repair members pagination

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -26,26 +26,15 @@ namespace API.Controllers
             _mapper = mapper;
         }
 
-        // [HttpGet]
-        // public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers([FromQuery]UserParams userParams)
-        // {
-        //     var gender = await _unitOfWork.UserRepository.GetUserGender(User.GetUsername());
-        //     userParams.CurrentUsername = User.GetUsername();
-
-        //     if (string.IsNullOrEmpty(gender))
-        //         userParams.Gender = gender == "male" ? "female" : "male";
-
-        //     var users = await _unitOfWork.UserRepository.GetMembersAsync(userParams);
-
-        //     Response.AddPaginationHeader(users.CurrentPage,users.PageSize,users.TotalCount,users.TotalPage);
-
-        //     return Ok(users);
-        // }
-
         [HttpGet]
-        public async Task<ActionResult<MemberDto>> GetUsers()
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers([FromQuery]UserParams userParams)
         {
-            var users = await _unitOfWork.UserRepository.GetUsersAsync();
+            userParams.CurrentUsername = User.GetUsername();
+
+            var users = await _unitOfWork.UserRepository.GetMembersAsync(userParams);
+
+            Response.AddPaginationHeader(users.CurrentPage,users.PageSize,users.TotalCount,users.TotalPage);
+
             return Ok(users);
         }
 
